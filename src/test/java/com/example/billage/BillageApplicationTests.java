eebb09c7ba709bb262c93ca3a4bbe47cd50541db
beomWhale <cndqjacndqja@gmail.com>
2022-06-14 15:26:17 +0900

    feat: set up the project

---
+++ src/test/java/com/example/billage/BillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.billage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
