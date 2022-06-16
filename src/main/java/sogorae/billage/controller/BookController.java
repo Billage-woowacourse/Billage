0b308a191fe08f30c8953bdcb749199ab6c092d9
beomWhale <cndqjacndqja@gmail.com>
2022-06-17 00:03:34 +0900

    feat: Controller 반납 상태 변환 기능 구현

--- src/main/java/sogorae/billage/controller/BookController.java
+++ src/main/java/sogorae/billage/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,5 +45,11 @@ public class BookController {
     public ResponseEntity<BookResponse> findOne(@PathVariable Long bookId) {
         BookResponse response = BookResponse.from(bookService.findById(bookId));
         return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{bookId}")
+    public ResponseEntity<BookResponse> returning(@PathVariable Long bookId, @AuthenticationPrincipal LoginMember loginMember) {
+        bookService.returning(bookId, loginMember.getEmail());
+        return ResponseEntity.ok().build();
     }
 }
